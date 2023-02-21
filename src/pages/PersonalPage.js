909cb0217d07e95c6f2caf816df8bc4302c10eb3
April Lee <april210608@gmail.com>
2023-02-21 12:34:30 -0800

    show the original date on profileEdit page

--- src/pages/PersonalPage.js
+++ src/pages/PersonalPage.js
@@ -23,55 +23,67 @@ export default function PersonalPage() {
 
   const call = () => {
     if (state.uid) {
-
       db.collection("user")
-        .where('userInfo.uid', '==', state.uid)
+        .where("userInfo.uid", "==", state.uid)
         .get()
         .then((result) => {
           result.forEach((doc) => {
             setUserInfo(doc.data().userInfo);
-          })
-        })
-
+          });
+        });
     }
-  }
+  };
 
   useEffect(() => {
     call();
-
   }, []);
-
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <button onClick={() => {
-          navigate(-1);
-        }} className={styles.back_btn}>&lt; Back</button>
+        <button
+          onClick={() => {
+            navigate(-1);
+          }}
+          className={styles.back_btn}
+        >
+          &lt; Back
+        </button>
         <div className={styles.slide}>
           <section className={styles.article_box}>
             <article className={styles.article}>
               <div className={styles.title_box}>
                 <div className={styles.article_title}>
-                  <div className={styles.article_big_profile_img} style={{ backgroundImage: `url('${userInfo.profileImage}')` }}></div>
-                  <span className={styles.article_profile_name}>{userInfo.name}</span>
+                  <div
+                    className={styles.article_big_profile_img}
+                    style={{
+                      backgroundImage: `url('${userInfo.profileImage}')`,
+                    }}
+                  ></div>
+                  <span className={styles.article_profile_name}>
+                    {userInfo.name}
+                  </span>
                 </div>
-                <button onClick={() => {
-                  navigate('/chatting', {
-                    state: {
-                      uid: userInfo.uid
-                      , name: userInfo.name
-                      , profileImage: userInfo.profileImage
-                    }
-                  });
-                }} className={styles.chat_btn}>Chat</button>
+                <button
+                  onClick={() => {
+                    navigate("/chatting", {
+                      state: {
+                        uid: userInfo.uid,
+                        name: userInfo.name,
+                        profileImage: userInfo.profileImage,
+                      },
+                    });
+                  }}
+                  className={styles.chat_btn}
+                >
+                  Chat
+                </button>
               </div>
               <div className={styles.introduce}>
-                <h4>{userInfo.city}, {userInfo.country}</h4>
-                <h5>
-                  32. Web Developer. BTS 💜 OT7. JK Biased. <br></br>Currently
-                  living in Vancouver. Let's chill!
-                </h5>
+                <h4>
+                  {userInfo.city}, {userInfo.country}
+                </h4>
+                <h5>{userInfo.intro}</h5>
               </div>
             </article>
           </section>
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,69 +1,117 @@
 /*eslint-disable */
 
-import React, { Profiler } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ProfileEdit.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import ProfileEditTop from '../components/ProfileEditTop';
+import ProfileEditTop from "../components/ProfileEditTop";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { db, storage } from "../index.js";
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/database";
 
 export default function ProfileEdit() {
   let navigate = useNavigate();
+
+  const [userName, setUserName] = useState("");
+  const [userIntro, setUserIntro] = useState("");
+  const [userCity, setUserCity] = useState("");
+  const [userCountry, setUserCountry] = useState("");
+  let [file, setFile] = useState();
+
+  let userNameShow = useSelector((state) => state.userNameShow);
+  let userCountryShow = useSelector((state) => state.userCountryShow);
+  let userCityShow = useSelector((state) => state.userCityShow);
+  let userIntroShow = useSelector((state) => state.userIntroShow);
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <button  onClick={() => {
+        <button
+          onClick={() => {
             navigate(-1);
-          }} className={styles.back_btn}>&lt; Back</button>
-       <ProfileEditTop></ProfileEditTop>
+          }}
+          className={styles.back_btn}
+        >
+          &lt; Back
+        </button>
+        <ProfileEditTop></ProfileEditTop>
         <div className={styles.slide}>
           <section className={styles.article_box}>
             <h3>Profile</h3>
             <section className={styles.edit_section}>
-            <div className={styles.btn_container}>
-                  <div className={styles.button_wrap}>
-                    <label className={styles.button} htmlFor='upload'>
-                      + Picture
-                    </label>
+              <div className={styles.btn_container}>
+                <div className={styles.button_wrap}>
+                  <label className={styles.button} htmlFor='upload'>
+                    + Picture
+                  </label>
 
-                    <input
-                      onChange={(e) => {
-                        setFile(e.target.files[0]);
-                      }}
-                      id='upload'
-                      type='file'
-                    ></input>
-                  </div>
+                  <input
+                    onChange={(e) => {
+                      setFile(e.target.files[0]);
+                    }}
+                    id='upload'
+                    type='file'
+                  ></input>
                 </div>
+              </div>
               <h6>Name</h6>
               <input
                 className={styles.edit_name}
                 type='text'
-                placeholder='Kelly'
+                defaultValue={userNameShow || userName}
+                onChange={(e) => {
+                  setUserName(e.target.value);
+                }}
               ></input>
               <h6>Introduction</h6>
               <textarea
                 className={styles.edit_name}
                 type='text'
-                placeholder='I am mad for headband. Jin is my true love!!!'
+                defaultValue={userIntroShow}
+                onChange={(e) => {
+                  setUserIntro(e.target.value);
+                }}
               ></textarea>
               <h6>City</h6>
               <input
                 className={styles.edit_city}
                 type='text'
-                placeholder='Vancouver'
+                defaultValue={userCityShow}
+                onChange={(e) => {
+                  setUserCity(e.target.value);
+                }}
               ></input>
               <h6>Country</h6>
               <input
                 className={styles.edit_country}
                 type='text'
-                placeholder='Canada'
+                defaultValue={userCountryShow}
+                onChange={(e) => {
+                  setUserCountry(e.target.value);
+                }}
               ></input>
             </section>
             <div className={styles.btn_section}>
-              <button  onClick={() => {
-            navigate('/mainboard');
-          }} className={styles.save_btn}>SAVE</button>
+              <button
+                onClick={() => {
+                  let changeUserInfo = {
+                    name: userName,
+                    intro: userIntro,
+                    city: userCity,
+                    country: userCountry,
+                  };
+                  db.collection("user")
+                    .doc()
+                    .update(changeUserInfo);
+                  navigate("/mainboard");
+                }}
+                className={styles.save_btn}
+              >
+                SAVE
+              </button>
             </div>
           </section>
         </div>
